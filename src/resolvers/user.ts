6928fc6ca9dd339c974503ac09ed69719ec2ded7
CWY <cwyoo01@naver.com>
2021-07-28 19:29:41 +0900

    studying

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,7 +1,11 @@
 import { User } from "../entities/User";
 import { MyContext } from "../types";
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from "argon2";
+
+// @~~는 지금부터 ~~를 정의할것임을 알려줌 @Resolver -> 지금부터 resolver 정의
+// @Field -> 쿼리 받았을 때 사용할 수 있는 Field 정의
+
 
 @InputType()
 class UsernamePasswordInput {
@@ -12,8 +16,18 @@ class UsernamePasswordInput {
     password: string
 }
 
-// @~~는 지금부터 ~~를 정의할것임을 알려줌 @Resolver -> 지금부터 resolver 정의
-// @Field -> 쿼리 받았을 때 보여줄 수 있는 Field 정의
+@ObjectType()
+class FieldError {
+
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [Error], {nullable:true})
+    errors?: Error[] // ?의 의미는 undefined라는 뜻
+    @Field(() => User, {nullable: true})
+    user?: User
+}
 
 // Resolver는 어떻게 이 graphQL이 작동하는지를 알려줌.
 @Resolver()
@@ -30,6 +44,25 @@ export class UserResolver {
             password: hashedPassword});
         await em.persistAndFlush(user);
         // Hashing을 통해 비번 암호화 해야 함. --> node-argon2 이용
+        return user;
+    }
+
+    @Mutation(() => User)
+    // hello라는 query가 들어오면 return "hello world"
+    async login(
+        @Arg("options") options: UsernamePasswordInput,
+        @Ctx() {em}: MyContext
+    ) {
+        // username이 기존DB에 있는지 없는지 확인
+        const user = await em.findOneOrFail(User, {username: options.username});
+        if (!user) {
+            return {
+                errors: [{
+                    
+                }]
+            }
+        }
+        const hashedPassword = await argon2.hash(options.password);
         return user;
     }
 }
